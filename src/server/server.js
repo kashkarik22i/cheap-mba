421f3c392f969e4a3f4f1bad7dc5ac461ef0fc3a
Ilya Kashkarev <ilya@ilyas-mbp.localdomain>
2020-11-29 12:05:06 +0100

    working version of admin

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+const rateLimit = require("express-rate-limit");
 
 const app = express();
-const port = 6000;
+const port = 8080;
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 miute window
+  max: 5, // start blocking after 5 requests
+  message:
+    "Too many requests sent from this IP, please try again after a while"
+});
+
+app.use("/api/", rateLimiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,6 +47,12 @@ app.post('/api/messages', (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+// should be after APIs or otherwise api GET gets lost
+app.use(express.static(path.join(__dirname, '..', '..', 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
